ad12d11b1038b49aa80ed8da7e64f2461845e7a8
valentino* <valentino@kali.kali.com>
2025-09-16 18:03:07 +0200

    new backend restaure and new db and passing user19

--- backend/server.js
+++ backend/server.js
@@ -315,15 +315,11 @@ app.get('/api/admin/get-seller-data/:dbName', hasUserAccess, async (req, res) =>
         const truckers = await Trucker.find();
         const approvisionnements = await Approvisionnement.find();
         
+        // CORRECTION: Récupérer tous les champs de l'attribution
         const history = truckers.flatMap(trucker =>
             trucker.gasoils.map(gasoil => ({
-                _id: gasoil._id,
+                ...gasoil.toObject(),
                 truckPlate: trucker.truckPlate,
-                liters: gasoil.liters,
-                date: gasoil.date,
-                operator: gasoil.operator,
-                name: gasoil.name,
-                activity: gasoil.activity
             }))
         );
 
